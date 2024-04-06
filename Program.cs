c4c18d70c6dbd25ddef9870e938259c0358f8877
elkhayyat <ahmed.elkhayyat@onlink4it.com>
2024-04-06 15:39:18 +0300

    my_task1

--- Program.cs
+++ Program.cs
@@ -10,16 +10,6 @@ if (permission.Contains("Admin"))
     {
         Console.WriteLine("Welcome, Admin user.");
     }
-}else if (permission.Contains("Manager"))
-{
-    if (level >= 20)
-    {
-        Console.WriteLine("Contact an Admin for access.");
-    }
-    else
-    {
-        Console.WriteLine("You do not have sufficient privileges.");
-    }
 }
 else
 {
